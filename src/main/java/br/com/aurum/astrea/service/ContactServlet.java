419b5b971765bea900aebd21b7947571cbedaa25
niccolas.costa <niccolas.costa@soft022-129.softplan.com.br>
2017-05-08 23:58:57 -0300

    implementando o ContactServlet

--- src/main/java/br/com/aurum/astrea/service/ContactServlet.java
+++ src/main/java/br/com/aurum/astrea/service/ContactServlet.java
@@ -1,34 +1,92 @@
 package br.com.aurum.astrea.service;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Objects;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
 import br.com.aurum.astrea.dao.ContactDao;
+import br.com.aurum.astrea.domain.Contact;
+import br.com.aurum.astrea.filter.ContactFilter;
+import br.com.aurum.astrea.util.JsonParse;
 
 @SuppressWarnings("serial")
 public class ContactServlet extends HttpServlet {
-	
+
+	public static final String ID = "id";
+	public static final String EMAIL_FILTER = "email";
+	public static final String CPF_FILTER = "cpf";
+	public static final String NAME_FILTER = "name";
+	public static final String ACTION_LISTAGEM = "list";
+	public static final String ACTION_GET_CONTACT = "get";
+	public static final String ACTION = "action";
+	private static final String UTF_8 = "UTF-8";
+	private static final String APPLICATION_JSON = "application/json";
 	private static final ContactDao DAO = new ContactDao();
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
-		// TODO: Implementar um método que irá ler o corpo da requisição e, com essas informações,
-		// salvar no banco de dados uma entidade do tipo 'Contato' com essas informações.
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+		String json = IOUtils.toString(req.getInputStream());
+		Contact contact = (Contact) JsonParse.convertJsonToObject(json, Contact.class);
+		if (Objects.isNull(contact)) {
+			return;
+		}
+		Long idContact = ContactServlet.DAO.save(contact);
+		if (Objects.isNull(contact.getId())) {
+			contact.setId(idContact);
+		}
+		this.setResponseData(resp, contact);
 	}
-	
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
-		// TODO: Implementar um método que irá listar todas as entidades do tipo 'Contato' e devolver para o client essa listagem.
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		this.setResponseData(resp, this.executeAction(req));
 	}
-	
+
 	@Override
-	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
-		// TODO: Implementar um método que irá deletar uma entidade do tipo 'Contato', dado parâmetro de identificação.
+	public void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		long contactId = NumberUtils.toLong(req.getParameter(ContactServlet.ID));
+		ContactServlet.DAO.delete(contactId);
+		this.setResponseData(resp, "OK");
+	}
+
+	private void setResponseData(HttpServletResponse resp, Object data) throws IOException {
+		resp.setContentType(ContactServlet.APPLICATION_JSON);
+		resp.setCharacterEncoding(ContactServlet.UTF_8);
+		PrintWriter out = resp.getWriter();
+		out.print(JsonParse.ObjectToJson(data));
+		out.flush();
+	}
+
+	private Object executeAction(HttpServletRequest req) {
+		String action = req.getParameter(ContactServlet.ACTION);
+		switch (action) {
+		case ACTION_LISTAGEM:
+			return this.listContacts(req);
+		case ACTION_GET_CONTACT:
+			return this.getContact(req);
+		default:
+			return null;
+		}
+	}
+
+	private Object getContact(HttpServletRequest req) {
+		Long contactId = NumberUtils.toLong(req.getParameter(ContactServlet.ID));
+		return ContactServlet.DAO.getContactById(contactId);
+	}
+
+	private Object listContacts(HttpServletRequest req) {
+		ContactFilter contactFilter = new ContactFilter();
+		contactFilter.setCpf(req.getParameter(ContactServlet.CPF_FILTER));
+		contactFilter.setEmail(req.getParameter(ContactServlet.EMAIL_FILTER));
+		contactFilter.setName(req.getParameter(ContactServlet.NAME_FILTER));
+		return ContactServlet.DAO.listByFilter(contactFilter);
 	}
 }
---
+++ src/main/java/br/com/aurum/astrea/util/JsonParse.java
@@ -0,0 +1,32 @@
+package br.com.aurum.astrea.util;
+
+import java.lang.reflect.Type;
+import java.util.Objects;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.gson.Gson;
+
+public class JsonParse {
+
+	public static Object convertJsonToObject(String json, Class<?> objectClass) {
+		if (StringUtils.isEmpty(json)) {
+			return null;
+		}
+		return new Gson().fromJson(json, objectClass);
+	}
+
+	public static Object convertJsonToObject(String json, Type objectType) {
+		if (StringUtils.isEmpty(json)) {
+			return null;
+		}
+		return new Gson().fromJson(json, objectType);
+	}
+
+	public static String ObjectToJson(Object object) {
+		if (Objects.isNull(object)) {
+			return null;
+		}
+		return new Gson().toJson(object);
+	}
+}
